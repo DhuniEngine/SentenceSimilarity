01b7b9a2fb23d3d83fde33d74e21de63a35dbe23
Singh <msingh@stcloudstate.edu>
2020-02-12 20:55:36 -0600

    Create jaroWinkler.py
    Implemented Jaro Winkler similarity measure. The results for the sample sentences are 80% similar

---
+++ jaroWinkler.py
@@ -0,0 +1,18 @@
+'''This code is going to demonstrate the working of
+Jaro-winkler similarity between 2 sentences. More information
+about Jaro-Winkler similarity can be obtained from the package 
+jellyfish'''
+
+import jellyfish
+
+def get_jaroWinkler_sim(str1, str2):
+    a = set(str1.split())
+    b = set(str2.split())
+    c = jellyfish.jaro_winkler(str(a),str(b))
+    return float(c)
+
+
+if __name__ == "__main__":
+    sentence_1 = "This sentence is vague"
+    sentence_2 = "This sentence is not clear"
+    print(get_jaroWinkler_sim(sentence_1, sentence_2))
